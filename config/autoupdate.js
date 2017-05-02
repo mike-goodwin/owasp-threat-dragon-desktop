91b578dad77835b29b51db49525fbd923b4d06ed
Mike Goodwin <mike.goodwin@owasp.org>
2017-05-02 21:03:58 +0100

    first pass autoupdate and installer

---
+++ config/autoupdate.js
@@ -0,0 +1,53 @@
+//adapted from https://github.com/crilleengvall/electron-tutorial-app
+
+const electron = require('electron')
+const app = electron.app
+
+module.exports = {
+    handleSquirrelEvent: function () {
+        if (process.argv.length === 1) {
+            return false;
+        }
+
+        const ChildProcess = require('child_process');
+        const path = require('path');
+
+        const appFolder = path.resolve(process.execPath, '..');
+        const rootAtomFolder = path.resolve(appFolder, '..');
+        const updateDotExe = path.resolve(path.join(rootAtomFolder, 'Update.exe'));
+        const exeName = path.basename(process.execPath);
+        const spawn = function (command, args) {
+            let spawnedProcess, error;
+
+            try {
+                spawnedProcess = ChildProcess.spawn(command, args, { detached: true });
+            } catch (error) { }
+
+            return spawnedProcess;
+        };
+
+        const spawnUpdate = function (args) {
+            return spawn(updateDotExe, args);
+        };
+
+        const squirrelEvent = process.argv[1];
+        switch (squirrelEvent) {
+            case '--squirrel-install':
+            case '--squirrel-updated':
+                // Install desktop and start menu shortcuts
+                spawnUpdate(['--createShortcut', exeName]);
+                setTimeout(app.quit, 1000);
+                return true;
+
+            case '--squirrel-uninstall':
+                // Remove desktop and start menu shortcuts
+                spawnUpdate(['--removeShortcut', exeName]);
+                setTimeout(app.quit, 1000);
+                return true;
+
+            case '--squirrel-obsolete':
+                app.quit();
+                return true;
+        }
+    }
+}
