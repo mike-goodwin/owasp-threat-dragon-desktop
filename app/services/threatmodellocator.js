aee37aaad7150ff21fa795c2961bba637af3e61e
Mike Goodwin <mike.goodwin@owasp.org>
2019-07-27 16:41:33 +0100

    fix load of demo model

--- app/services/threatmodellocator.js
+++ app/services/threatmodellocator.js
@@ -12,42 +12,40 @@ function threatmodellocator() {
 
     return service;
 
-    //gets a file path from a uri encoded value
+    //gets a file path from a base64 encoded value
     //repeatedely decode until it looks like an unencoded value
+    //ignore demo
     function getModelLocation(params) {
 
         var result = params.location;
 
-        while (checkEncoded(result)) {
-            result = atob(result);
+        if (result != 'demo') {
+            while (checkEncoded(result)) {
+                result = atob(result);
+            }
         }
 
         return result;
     }
 
     //gets a url path from a file path
-    //encode it unless it already looks encoded
+    //base64 encode it unless it already looks encoded
+    //ignore demo
     function getModelPath(location) {
 
         var result = location;
 
-        if (!checkEncoded(result)) {
-            result = btoa(result);
+        if (result != 'demo') {
+            if (!checkEncoded(result)) {
+                result = btoa(result);
+            }
         }
 
         return result;
     }
 
-    //get a url path from a route params object
-    //encode it unless it already looks encoded
     function getModelPathFromRouteParams(params) {
-        var result = params.location;
-
-        if (!checkEncoded(result)) {
-            result = btoa(result);
-        }
-
-        return result;
+        return getModelPath(params.location);
     }
 
     function willMoveModel() {
